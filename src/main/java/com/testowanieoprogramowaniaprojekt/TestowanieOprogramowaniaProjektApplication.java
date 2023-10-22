fdb36e988553abf82fe1a2bd1853bd4fc9ce9256
Kacper Sosnowski <01169627@pw.edu.pl>
2023-10-22 17:12:37 +0200

    Created spring project

---
+++ src/main/java/com/testowanieoprogramowaniaprojekt/TestowanieOprogramowaniaProjektApplication.java
@@ -0,0 +1,13 @@
+package com.testowanieoprogramowaniaprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestowanieOprogramowaniaProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestowanieOprogramowaniaProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testowanieoprogramowaniaprojekt/TestowanieOprogramowaniaProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testowanieoprogramowaniaprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestowanieOprogramowaniaProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
