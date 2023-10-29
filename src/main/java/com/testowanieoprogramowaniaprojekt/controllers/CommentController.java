81fb46505457c83660fc8cdda049dcfe79ecaae6
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-10-29 11:13:29 +0100

    Comment API added

---
+++ src/main/java/com/testowanieoprogramowaniaprojekt/controllers/CommentController.java
@@ -0,0 +1,59 @@
+package com.testowanieoprogramowaniaprojekt.controllers;
+
+import com.testowanieoprogramowaniaprojekt.entities.Comment;
+import com.testowanieoprogramowaniaprojekt.exceptions.BadRequestException;
+import com.testowanieoprogramowaniaprojekt.services.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "comments")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleException(BadRequestException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Comment>> getAllComments() {
+        return new ResponseEntity<>(commentService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Comment> getCommentById(@PathVariable("id") long id) {
+        return new ResponseEntity<>(commentService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) {
+        if(comment.getContent() == null || comment.getContent().trim().isEmpty()) {
+            throw new BadRequestException("Comment content is mandatory.");
+        } else if(comment.getAuthor() == null) {
+            throw new BadRequestException("Comment author is mandatory.");
+        }
+        return new ResponseEntity<>(commentService.save(comment), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Comment> updateComment(@PathVariable("id") long id, @RequestBody Comment comment) {
+        if(comment.getContent() == null || comment.getContent().trim().isEmpty()) {
+            throw new BadRequestException("Comment content is mandatory.");
+        } else if(comment.getAuthor() == null) {
+            throw new BadRequestException("Comment author is mandatory.");
+        }
+        return new ResponseEntity<>(commentService.update(id, comment), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteComment(@PathVariable Long id) {
+        commentService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/testowanieoprogramowaniaprojekt/entities/Comment.java
@@ -0,0 +1,38 @@
+package com.testowanieoprogramowaniaprojekt.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    @Column(name = "content")
+    @NotEmpty(message = "Comment content is mandatory.")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User author;
+
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "post_id", nullable = false)
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    @JsonIgnore
+//    private Post post;
+}
