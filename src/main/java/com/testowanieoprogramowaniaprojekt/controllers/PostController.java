40d325d7a9c90a3f1638f8838dec7c218400c51e
Robert Małafiejew <01169593@pw.edu.pl>
2023-11-08 00:32:42 +0100

    Added swagger (not working yet)

--- src/main/java/com/testowanieoprogramowaniaprojekt/controllers/PostController.java
+++ src/main/java/com/testowanieoprogramowaniaprojekt/controllers/PostController.java
@@ -2,40 +2,89 @@ package com.testowanieoprogramowaniaprojekt.controllers;
 
 import com.testowanieoprogramowaniaprojekt.entities.Post;
 import com.testowanieoprogramowaniaprojekt.services.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
 @RequestMapping("posts")
+@Tag(name = "Post", description = "Posts management APIs")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
+    @Operation(
+            summary = "Find all Posts",
+            description = "Get all Post instances."
+    )
     @GetMapping
     Collection<Post> findAll() {
         return postService.findAll();
     }
 
+    @Operation(
+            summary = "Find Post by id",
+            description = "Get a Post by specifying its id."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            content = {
+                    @Content(schema = @Schema(implementation = Post.class), mediaType = "application/json")})
+    @ApiResponse(
+            responseCode = "404",
+            content = {@Content(schema = @Schema())})
+    @ApiResponse(
+            responseCode = "500",
+            content = {@Content(schema = @Schema())})
     @GetMapping("/{id}")
-    Post findById(@PathVariable Long id) {
+    Post findById(
+            @Parameter(description = "Post id.", example = "1")
+            @PathVariable Long id
+    ) {
         return postService.findById(id);
     }
 
+    @Operation(
+            summary = "Post a Post",
+            description = "Post a Post to database."
+    )
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     Post create(@RequestBody Post post) {
         return postService.save(post);
     }
 
+    @Operation(
+            summary = "Update Post",
+            description = "Put a Post by specifying its id and providing new Post."
+    )
     @PutMapping("/{id}")
-    Post update(@RequestBody Post post, @PathVariable Long id) {
+    Post update(
+            @RequestBody Post post,
+            @Parameter(description = "Post id.", example = "1")
+            @PathVariable Long id
+    ) {
         return postService.update(id, post);
     }
 
+    @Operation(
+            summary = "Delete Post by id",
+            description = "Delete a Post by specifying its id."
+    )
     @DeleteMapping("/{id}")
-    void delete(@PathVariable Long id) {
+    void delete(
+            @Parameter(description = "Post id.", example = "1")
+            @PathVariable Long id
+    ) {
         postService.deleteById(id);
     }
 }
