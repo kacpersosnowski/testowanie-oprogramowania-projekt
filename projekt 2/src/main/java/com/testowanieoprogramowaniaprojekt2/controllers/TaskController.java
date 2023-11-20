f69be3217588e8e48d5f4e5e14fdd8eb462d9960
Robert Małafiejew <rmalafiej@gmail.com>
2023-11-20 15:20:58 +0100

    Added swagger to TaskController

--- projekt 2/src/main/java/com/testowanieoprogramowaniaprojekt2/controllers/TaskController.java
+++ projekt 2/src/main/java/com/testowanieoprogramowaniaprojekt2/controllers/TaskController.java
@@ -2,6 +2,10 @@ package com.testowanieoprogramowaniaprojekt2.controllers;
 
 import com.testowanieoprogramowaniaprojekt2.entities.Task;
 import com.testowanieoprogramowaniaprojekt2.services.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,21 +23,42 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @Operation(summary = "Get Task with specifying its id.")
+    @ApiResponse(
+            responseCode = "200",
+            content = {
+                    @Content(schema = @Schema(implementation = Task.class), mediaType = "application/json")})
+    @ApiResponse(
+            responseCode = "404",
+            content = {@Content(schema = @Schema())})
+    @ApiResponse(
+            responseCode = "500",
+            content = {@Content(schema = @Schema())})
     @GetMapping("/{id}")
     Task getById(@PathVariable Long id) {
         return taskService.getTaskById(id);
     }
 
+    @Operation(summary = "Get all completed tasks.")
     @GetMapping("/completed")
     List<Task> getCompleted() {
         return taskService.getCompletedTasks();
     }
 
+    @Operation(summary = "Get all uncompleted tasks.")
     @GetMapping("/uncompleted")
     List<Task> getUncompleted() {
         return taskService.getUncompletedTasks();
     }
 
+    @Operation(summary = "Get all tasks with provided priority.")
+    @ApiResponse(
+            responseCode = "200",
+            content = {
+                    @Content(schema = @Schema(implementation = Task.class), mediaType = "application/json")})
+    @ApiResponse(
+            responseCode = "400",
+            content = {@Content(schema = @Schema())})
     @GetMapping("/filter/priority/{priority}")
     List<Task> getFilteredByPriority(@PathVariable int priority) {
         return taskService.getByPriority(priority);
