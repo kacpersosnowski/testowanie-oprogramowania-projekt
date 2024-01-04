a2ac56495cd835cf63a08605d99ddd548c31b398
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2024-01-04 22:58:24 +0100

    Old cucumber stuff

--- projekt 2/src/test/java/com/testowanieoprogramowaniaprojekt2/RunCucumberTest.java
+++ projekt 2/src/test/java/com/testowanieoprogramowaniaprojekt2/RunCucumberTest.java
@@ -10,6 +10,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/testowanieoprogramowaniaprojekt2")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value="pretty, html:target/cucumber-reports/Cucumber.html")
 public class RunCucumberTest {
 }
